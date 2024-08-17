aa8e7216782d60aa04a254f0fc8418af0c934ae3
Barrett Otte <barrettotte@gmail.com>
2024-08-17 16:51:11 -0400

    switch build to cmake and conan

--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 
 Tetris clone in C and compiled to WASM.
 
+This was made to learn the basics of WASM, CMake, and Conan.
+
 ## Usage
 
 TODO:
+
+```sh
+# init build system
+conan install . --output-folder=build/ --build=missing
+cmake -S . -B build tetris
+
+# build
+make -C build tetris
+
+# run
+./bin/tetris
+```
 
 ## References
 
--- src/tetris.c
+++ src/tetris.c
@@ -1,9 +1,47 @@
-#include <stdio.h>
-
+#include "raylib.h"
 #include "tetris.h"
 
+#include <stdio.h>
+
+// constants
+static const int screenWidth = 800;
+static const int screenHeight = 450;
+
+// prototypes
+
+static void init();
+static void update();
+static void draw();
+
+
+// entry
 int main(int argc, const char* argv[]) {
-    printf("Hello world\n");
+    printf("Tetris started.\n");
+    InitWindow(screenWidth, screenHeight, "Tetris");
+    init();
+
+    while (!WindowShouldClose()) {
+        update();
+        draw();
+    }
+
+    CloseWindow();
+    printf("Tetris ended.\n");
 
     return 0;
+}
+
+// set initial game state
+static void init() {
+    SetTargetFPS(60);
+}
+
+// process updates during game loop
+static void update() {
+    
+}
+
+// draw updates during game loop
+static void draw() {
+
 }
